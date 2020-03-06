65ee2093701a8f8b09faba2e3be5386bc3ec3b4b
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-03-06 09:34:46 +0100

    Debut ICT151

---
+++ .constant.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Programme : Exo Php
+ * Auteur : Thomas Grossmann
+ * Date : 07.02.2020
+ */
+
+$user = "phpApplication";
+$pass = 'Pa$$w0rd';
+$dbhost = 'localhost';
+$dbname = 'snows';
+
+
+?>
--- model/model.php
+++ model/model.php
@@ -5,22 +5,58 @@
  * Date : 21.01.2020
  */
 
+//Fonction permettant de se connecter à la base de données
+function getPDO()
+{
+    require ".constant.php";
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+}
+
 //Fonction permettant de récupérer les données des news depuis le fichier news.json
 function getNews()
 {
-    return json_decode(file_get_contents("model/dataStorage/news.json"),true);
+    try {
+        $query = 'SELECT * FROM news';
+        $statement = getPDO()->prepare($query);//prepare query
+        $statement->execute();//execute query
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);//prepare result for client
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+    //return json_decode(file_get_contents("model/dataStorage/news.json"),true);
 }
 
 //Fonction permettant de récupérer les données des snowboards depuis le fichier snows.json
 function getSnows()
 {
-    return json_decode(file_get_contents("model/dataStorage/snows.json"), true);
+    try {
+        $query = 'SELECT * FROM snows';
+        $statement = getPDO()->prepare($query);
+        $statement->execute();
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+    //return json_decode(file_get_contents("model/dataStorage/snows.json"), true);
 }
 
 //Fonction permetttant de récupérer les données des users depuis le fichier Users.json
 function getUsers()
 {
-    return json_decode(file_get_contents("model/dataStorage/Users.json"), true);
+    try {
+        $query = 'SELECT * FROM users';
+        $statement = getPDO()->prepare($query);
+        $statement->execute();
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+    }
+    //return json_decode(file_get_contents("model/dataStorage/Users.json"), true);
 }
 
 //Fonction permettant de ressortir un utilisateur via son username afin de vérifier la connection
